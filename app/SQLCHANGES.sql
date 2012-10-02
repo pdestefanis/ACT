8e865f7917ea908133bf445fc3fc5a57c6ce64b5
L Hristov <lkhristov@gmail.com>
2012-10-02 10:49:18 +0200

    fix

--- app/SQLCHANGES.sql
+++ app/SQLCHANGES.sql
@@ -1,7 +1,7 @@
  --Store chagnes to database here
  alter table stats add column modified datetime;
  --generic patient
- insert into patients (number, consent) values (P999999, 1);
+ insert into patients (number, consent) values ('P999999', 1);
  --add comment field
  alter table units add column comment varchar(255);
  --soft deleted for units
